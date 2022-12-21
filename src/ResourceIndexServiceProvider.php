ba47036dee310f2ebdedec793420fc5f0ca22dd6
sem-feldoe <sem-feldoe@users.noreply.github.com>
2022-12-21 14:18:24 +0000

    Fix styling

--- src/ResourceIndexServiceProvider.php
+++ src/ResourceIndexServiceProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Atx\ResourceIndex;
 
-use Illuminate\Support\ServiceProvider;
 use Atx\ResourceIndex\Contracts\ResourceIndex as ResourceIndexContract;
+use Illuminate\Support\ServiceProvider;
 
 class ResourceIndexServiceProvider extends ServiceProvider
 {
